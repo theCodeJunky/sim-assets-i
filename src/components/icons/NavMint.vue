8b0c422e8696a9cc3615ccb75c60eba47290052c
thecodejunky <thecodejunky@gmail.com>
2023-07-11 10:25:42 -0700

    Minor SVG cleanup

--- src/components/icons/NavMint.vue
+++ src/components/icons/NavMint.vue
@@ -1,15 +1,6 @@
 <template>
-  <!-- <svg class="background" width="26" height="31" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M23.8616 5.964 2.8253 16.345l7 5.5269 7.662 5.985 6.3743-21.8927Z" stroke="#fff" stroke-width="1.5"/>
-    <path d="m9.8699 24.2604-1.0833 3.1941 2.0328-2.5509M20.1006 10.5743l-8.515 9.8972" stroke="#fff" stroke-linecap="round"/>
-  </svg> -->
-  <svg class="background" width="26" height="31" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <svg width="26" height="31" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M23.862 5.964 2.825 16.344l7 5.528 7.662 5.985 6.375-21.893Z" stroke="#fff" stroke-width="1.5"/>
     <path d="m9.87 24.26-1.083 3.194 2.032-2.55M20.1 10.574l-8.514 9.897" stroke="#fff" stroke-linecap="round"/>
   </svg>
 </template>
-<style scoped>
-  .background {
-    background-color: pink;
-  }
-</style>
